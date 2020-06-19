5f2223ff8eea8fbba099ee2fdffc79886d473696
Linh Tran <linh.tran@episerver.com>
2020-06-19 15:52:42 +0700

    init

---
+++ websitesracing/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace websitesracing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public class Customer
+        {
+            public string CustName { get; set; }
+            public string CustEmail { get; set; }
+        }
+
+        [HttpPost]
+        public JsonResult CreateCustomer(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Data save to database  
+                return Json(new
+                {
+                    success = true
+                });
+            }
+            return Json(new
+            {
+                success = false,
+                errors = ModelState.Keys.SelectMany(i => ModelState[i].Errors).Select(m => m.ErrorMessage).ToArray()
+            });
+        }
+    }
+}
---
+++ websitesracing/ControllersNg/CustomerController.js
@@ -0,0 +1,42 @@
+﻿angular.module('myFormApp', []).controller('CustomerController', function ($scope, $http, $location, $window) {
+    $scope.cust = {};
+    $scope.message = '';
+    $scope.result = "color-default";
+    $scope.isViewLoading = false;
+    //get called when user submits the form
+    $scope.submitForm = function () {
+        $scope.isViewLoading = true;
+        console.log('Form is submitted with:', $scope.cust);
+        //$http service that send or receive data from the remote server
+        $http({
+            method: 'POST',
+            url: '/Home/CreateCustomer',
+            data: $scope.cust
+        }).then(function onSuccess(data) {
+            $scope.errors = [];
+            console.log(data);
+            if (data.data.success === true) {
+                $scope.cust = {};
+                $scope.message = 'Form data Submitted!';
+                $scope.result = "color-green";
+                $location.path(data.redirectUrl);
+                $window.location.reload();
+            }
+            else {
+                $scope.errors = data.errors;
+            }
+        }).catch(function onError(data, status, headers, config) {
+            $scope.errors = [];
+            $scope.message = 'Unexpected Error while saving data!!';
+        });
+
+        $scope.isViewLoading = false;
+    }
+}).config(function ($locationProvider) {
+    //default = 'false'
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
+
